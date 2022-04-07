8ed7f4419d2b1df8a12c658ed67902f3827e3c4a
Duo Zhang <duozhang@iastate.edu>
2022-04-07 00:08:14 -0500

    backwrad slicing

---
+++ build/CMakeFiles/Drchecker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Drchecker.dir/BackwardSlicing.cpp.o"
+  "libDrchecker.pdb"
+  "libDrchecker.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Drchecker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
