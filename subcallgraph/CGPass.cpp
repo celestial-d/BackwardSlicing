57540bb3884b5358fef583f47bd0722fccd99008
Duo Zhang <duozhang@iastate.edu>
2022-05-12 00:30:48 -0500

    finish subgraph of a node

---
+++ subcallgraph/CGPass.cpp
@@ -0,0 +1,88 @@
+#include "CGPass.h"
+#include <iostream>
+#include <fstream>
+#include <deque>
+
+void CallGraph::dump() const {
+	std::error_code error;
+	enum sys::fs::OpenFlags F_None;
+	StringRef fileName("cg.dot");
+	raw_fd_ostream file(fileName, error, F_None);
+	std::string dotString = "";
+	file << "digraph \"Call Graph\"{\n";
+	file << "label=" << target << ";\n";
+	for (Function* f : valueList) {
+		file << "Node" << f << " [shape=record, label=\"{" << f->getName() << "}\"];\n";
+	}
+	for (auto pairs : m_map) {
+		for (Function* second : pairs.second) {
+			file << "Node" << pairs.first << " -> " << "Node" << second << ";\n";
+		}
+	}
+	file <<  "}\n";
+	file.close();
+}
+
+
+
+bool CGPass::runOnModule(Module &M) {
+
+	//Function *main = M.getFunction("dax_visible");
+	Function *main = M.getFunction(target);
+	G = new CallGraph(main);
+	G->valueList.push_back(main);
+	if (!main) return false;
+
+	std::deque<Function*> list;
+
+	list.push_back(main);
+
+	while (!list.empty()) {
+		Function* func = list.front();
+		list.pop_front();
+		for (Function::iterator iter = func->begin(); iter != func->end(); ++iter)
+		{
+
+			for (BasicBlock::iterator Biter = iter->begin(); Biter != iter->end(); ++Biter)
+			{
+				Instruction *I = &*Biter;
+				if (CallInst *inst = dyn_cast<CallInst>(I))
+				{
+
+					Function* called = inst->getCalledFunction();
+					if (called)
+					{
+
+						G->AddEdge(func, called);
+						if (!G->hasFunction(called))
+						{
+							list.push_back(called);
+							G->valueList.push_back(called);
+						}
+					}
+				}
+				if (InvokeInst *inst = dyn_cast<InvokeInst>(I))
+				{
+					Function* called = inst->getCalledFunction();
+					errs() << "hello\n";
+					if (called)
+					{
+						G->AddEdge(func, called);
+						if (!G->hasFunction(called))
+						{
+							list.push_back(called);
+							G->valueList.push_back(called);
+						}
+					}
+				}
+			}
+		}
+	}
+	G->dump();
+
+}
+
+
+
+//pass register
+static RegisterPass<CGPass> X("CGPass", "Call Graph Pass", false, false);
---
+++ subcallgraph/CGPass.h
@@ -0,0 +1,56 @@
+#include <llvm/Pass.h>
+#include <llvm/IR/Function.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/InstIterator.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Metadata.h>
+#include <llvm/IR/DebugInfo.h>
+#include <llvm/IR/DebugLoc.h>
+#include <llvm/IR/IntrinsicInst.h>
+#include <llvm/Analysis/AliasAnalysis.h>
+#include <string>
+#include <set>
+#include "llvm/Support/CommandLine.h"
+using namespace llvm;
+
+namespace {
+	static cl::opt<std::string> target("target",cl::desc("target funcion"),cl::value_desc("target"), cl::init(""));
+	class CallGraph {
+	public:
+		
+		CallGraph(Function *mainFunc) : mMain(mainFunc) {}
+		~CallGraph() {}
+		void AddEdge(Function * f, Function * t) {
+			m_map[f].insert(t);
+		}
+		//print call graph
+		void print() const;
+		//save to dot
+		void dump() const;
+		bool hasFunction(Function *func) {
+			std::map<Function*, std::set<Function*> >::iterator iter = m_map.find(func);
+			if (iter == m_map.end())
+				return false;
+			return true;
+		}
+		std::map<Function*, std::set<Function*> > m_map;
+		std::list<Function*> valueList;
+	private:
+		Function* mMain; 
+		unsigned int edge_count = 0;
+	};
+	class CGPass : public ModulePass
+	{
+	public:
+		static char ID;
+		CGPass() : ModulePass(ID){}
+		virtual bool runOnModule(Module &M) override;
+		
+	private:
+		CallGraph *G;		
+	};
+	char CGPass::ID = 0;
+}
+
